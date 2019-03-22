a24959beecc1815bcca43ca510fbe9124baae63e
keshan123 <keshan.peiris@gmail.com>
2019-03-22 11:07:13 +1100

    fix container height

--- src/components/ResultsList/ResultsListContainer.js
+++ src/components/ResultsList/ResultsListContainer.js
@@ -4,6 +4,6 @@ export const ResultsListContainer = styled.ul`
   background: white;
   box-shadow: 0.5rem 0.5rem 1rem hsl(0, 0%, 90%);
   list-style: none;
-  margin: -5px 38px 0 88px;
+  margin: 0 38px 0 88px;
   padding: 0;
 `;
